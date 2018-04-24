b466e45ad46f6457c3393535a2d2286ab04e417f
Joel Adams, Sara Hamilton <joeladamsdesign@gmail.com, sara@indeterminate.net>
2018-04-24 16:08:05 -0700

    add index view and controller for categories

---
+++ ToDoList5/Controllers/CategoriesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList5.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ToDoList5.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private ToDoListContext db = new ToDoListContext();
+        public IActionResult Index()
+        {
+            List<Category> model = db.Categories.ToList();
+            return View(model);
+        }
+    }
+}
