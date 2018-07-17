b60110c5c74f3a84a48d8e95db8030204e4621fc
Sara <sara@indeterminate.net>
2018-07-17 16:53:33 -0700

    change db connection string

--- ToDoList5/Models/ToDoListContext.cs
+++ ToDoList5/Models/ToDoListContext.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 
 namespace ToDoList5.Models
 {
@@ -18,7 +20,12 @@ namespace ToDoList5.Models
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseMySql(@"Server=localhost;Port=8889;database=todoazure;uid=root;pwd=root;");
+            //optionsBuilder.UseMySql(@"Server=localhost;Port=8889;database=todoazure;uid=root;pwd=root;");
+            //optionsBuilder.UseMySql(@"Server = tcp:webapplication120180717093406dbserver.database.windows.net,1433; Initial Catalog = ToDoAzure20180717110030_db; Persist Security Info = False; User ID = {SaraHamilton}; Password = {33kr8-)6SUpz721}; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30;");
+            optionsBuilder.UseMySql(@"Server=tcp:webapplication120180717093406dbserver.database.windows.net,1433; Initial Catalog = ToDoAzure20180717110030_db; User Id = SaraHamilton@webapplication120180717093406dbserver; Password = 33kr8-)6SUpz721;");
+
+
+            //optionsBuilder.UseMySql(connectionString: @"DefaultConnection");
         }
 
         public ToDoListContext(DbContextOptions<ToDoListContext> options)
